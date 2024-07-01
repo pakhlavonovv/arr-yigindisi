1f527a9937c1217a411ce3d3d78387a2383bd2d9
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-07-02 02:16:01 +0500

    arr-yigindi

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let arr = [22 , 33 , 22 , 33]
+let f_item = arr[0]
+let s_item = arr[1]
+let th_item = arr[2]
+let fth_item = arr[3]
+let result = f_item + s_item + th_item + fth_item;
+alert(`Arrayni ichidagi sonlar yig'indisi : ${result}`)
